f06913616030217ccd937b330f2b2c411593ec43
Syed Amaan Mehmoodi <146960899+amaan4n@users.noreply.github.com>
2023-11-02 17:55:51 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,97 @@
+print("hello World 1")
+#it is single comment
+""" 
+this is multi line comment
+hccfubi
+hgchgbjn
+"""
+#variable
+number = 26
+print(number)
+
+myNameIs = "amaan"
+word = 'n'
+
+print(myNameIs)
+print(word)
+
+#datatype
+
+num = 99 #integer
+alphabet = "sam" #string
+num1 = 99.0000 #float
+bool = True #boolean
+type(bool)
+print(type(bool))
+
+#casting-changing data type into another data type
+number = 26
+temp = float(number)
+print(temp)
+
+number = 26.99
+temp = int(number)
+print(temp)
+
+sam = "26"
+temp = float(sam)
+print(temp)
+
+sam1 = 26
+temp = str(sam1)
+print(temp)
+print(type(sam1))
+#
+string2 = "amaan"
+string3 = """
+hbjbkj
+jjnkmkn
+bjnkj
+9u989yiuh
+"""
+print(string2)
+print(string3)
+print(string2[4])#index number
+#
+value = "my name is amaan"
+print("my" in value)
+#
+value1 = "helloworld"
+print(value1[5:10])
+
+value2 = "hello jfdnvjnvovonnnkk"
+print(value2[6:])
+
+value3 = "sam"
+print(value3.upper())
+#
+value1 = "SAM"
+value2 = "AMAAN"
+
+temp=value1+" "+value2
+print(temp)
+#
+value1 = "AMAAN"
+print(value1.startswith("A"))
+
+value2 = "AMAAN"
+print(value2.endswith("N"))
+#
+value1 = 1
+value2 = 3
+print(value1>value2)
+#false=0
+#true=1
+
+#
+#conditional statements
+#if else
+value1 = 44
+value2 = 67
+
+if value1 < value2:#less than
+    print("true")
+else:
+    print("false")
+
+
